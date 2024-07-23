4309c9cd2d45c029e33d080a3eba80d020980b59
Petro Kuida <petr.k1201@gmail.com>
2024-07-23 17:43:11 +0100

    Add code to start the game with selected difficulty level and disable Start button when game launched

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const gameBoard = document.getElementById('game-board');
-    const startBtn = document.getElementById('start-btn');
-    const difficultySelect = document.getElementById('difficulty');
-
     startBtn.addEventListener('click', runGame);
+})
+
+const startBtn = document.getElementById('start-btn');
+function runGame() {
+    const gameBoard = document.getElementById('game-board');
+    const difficultySelect = document.getElementById('difficulty');
+    const gridSize = parseInt(difficultySelect.value);
+    numbers = shuffle(createNumbersArray(gridSize * gridSize));
+    gameBoard.innerHTML = '';
+    currentNumber = 1;
+    createBoard();
+    startBtn.disabled = true;
+    startTimer();
 }
 
-
-
-function runGame() {
-
+function createNumbersArray(size) {
+    const array = [];
+    for (let i = 1; i <= size; i++) {
+        array.push(i);
+    }
+    return array;
 }
 
 function createBoard() {
