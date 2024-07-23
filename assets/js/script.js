8b9215700443439a33e6dc73d3bd82fe93810754
Petro Kuida <petr.k1201@gmail.com>
2024-07-23 16:53:36 +0100

    Add event listners for document loading and Start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const gameBoard = document.getElementById('game-board');
+    const startBtn = document.getElementById('start-btn');
+    const difficultySelect = document.getElementById('difficulty');
+
+    startBtn.addEventListener('click', runGame);
+}
+
+
+
 function runGame() {
 
 }
