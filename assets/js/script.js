0647770da97bf02fb08daeb32df40ad3f5f3b85d
Petro Kuida <petr.k1201@gmail.com>
2024-07-23 17:55:46 +0100

    Move all code inside document.addEventListener('DOMContentLoaded', ...) to ensure it runs only after the DOM is fully loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-    startBtn.addEventListener('click', runGame);
-})
-
-const startBtn = document.getElementById('start-btn');
-function runGame() {
     const gameBoard = document.getElementById('game-board');
     const difficultySelect = document.getElementById('difficulty');
     const gridSize = parseInt(difficultySelect.value);
-    numbers = shuffle(createNumbersArray(gridSize * gridSize));
-    gameBoard.innerHTML = '';
-    currentNumber = 1;
-    createBoard();
-    startBtn.disabled = true;
-    startTimer();
-}
+    const startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', runGame);
 
-function createNumbersArray(size) {
-    const array = [];
-    for (let i = 1; i <= size; i++) {
-        array.push(i);
+    function runGame() {
+        numbers = shuffle(createNumbersArray(gridSize * gridSize));
+        gameBoard.innerHTML = '';
+        currentNumber = 1;
+        createBoard();
+        startBtn.disabled = true;
+        startTimer();
     }
-    return array;
-}
 
-function createBoard() {
+    function createNumbersArray(size) {
+        const array = [];
+        for (let i = 1; i <= size; i++) {
+            array.push(i);
+        }
+        return array;
+    }
 
-}
+    function createBoard() {
 
-function handleCellClick() {
+    }
 
-}
+    function handleCellClick() {
 
-function shuffle() {
+    }
 
-}
+    function shuffle() {
 
-function startTimer() {
+    }
 
-}
+    function startTimer() {
 
-function updateHighScore() {
+    }
 
-}
+    function updateHighScore() {
+
+    }
+})
